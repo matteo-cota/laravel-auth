f19a6c6e1944a48b54b54b7f1b5a053d17d0f595
matteo-cota <matteocota@outlook.it>
2024-11-04 19:08:11 +0100

    added seeder projectseeder - migrate

--- database/migrations/2024_11_04_180425_create_projects_table.php
+++ database/migrations/2024_11_04_180425_create_projects_table.php
@@ -9,13 +9,17 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
+
 
     /**
      * Reverse the migrations.
